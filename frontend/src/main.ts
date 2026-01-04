b5c8c029aa40a228ab1313e14c9b50670c859645
Gianni Carafa <gianni.carafa@srf.ch>
2026-01-04 11:44:17 +0100

    migrate CSS to tailwind4

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
 import App from './App.vue'
 
+// Tailwind CSS
+import './index.css'
+
 // PrimeVue CSS
 import 'primevue/resources/themes/lara-dark-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
-
-// Custom CSS
-import './assets/main.css'
 
 const app = createApp(App)
 
