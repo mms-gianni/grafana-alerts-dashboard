abd88f794463b23f71dbc1b8edd400f4207de1e5
Gianni Carafa <gianni.carafa@srf.ch>
2026-01-07 22:42:57 +0100

    Minor fix

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_BACKEND_URL: string
+  readonly VITE_BACKEND_URL?: string
+  readonly PROD: boolean
 }
 
 interface ImportMeta {
