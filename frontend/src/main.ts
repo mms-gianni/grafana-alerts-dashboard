316e34bc5fa64529ba2a3ee13a70738c9a624edc
Gianni Carafa <gianni.carafa@srf.ch>
2026-01-16 16:38:11 +0100

    chore: upgrade packages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,17 +1,24 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
 import App from './App.vue'
 
 // Tailwind CSS
 import './index.css'
 
-// PrimeVue CSS
-import 'primevue/resources/themes/lara-dark-blue/theme.css'
-import 'primevue/resources/primevue.min.css'
+// PrimeIcons
 import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
-app.use(PrimeVue)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: '.dark',
+      cssLayer: false
+    }
+  }
+})
 
 app.mount('#app')
